2a0cdfbb3b1a3d7be229d8d83d2ec4c58f219df2
DicedPingu <PenguinOnDice@outlook.com>
2023-04-01 00:40:22 +0200

    Update commented.py

--- commented.py
+++ commented.py
@@ -97,3 +97,15 @@ def prime_maker_sieve(limit: int) -> np.ndarray:
             prime_map[i ** 2::i] = 0
     # Return the numpy array of all prime numbers up to the limit
     return np.nonzero(prime_map)[0]
+
+
+primes_to_make = 2500
+# OG function, finds 2500 primes in a couple of seconds.
+print(np.fromiter(prime_maker_first(primes_to_make), dtype=np.int32))
+# Function 2, finds 20 times the amount of primes, using the same time.
+print(np.fromiter(prime_maker_second(primes_to_make * 20), dtype=np.int32))
+# Other function, only 12 times faster than OG
+print(np.fromiter(prime_maker_np(primes_to_make * 12), dtype=np.int32))
+# Total beast function, you have to calculate the number you want primes up to
+# The number needed to reach a certain amount of primes is 'n * (log(n) + log(log(n)))'
+print(prime_maker_sieve(int(primes_to_make*(log(primes_to_make) + log(log(primes_to_make)))) * 8000))
